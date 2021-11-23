990452fbc58ed080e7e0657005dc6108d267634c
Alex Macniven <alex_macniven@icloud.com>
2021-11-23 10:20:04 +0000

    Add Core Functionality

--- postman_py/__init__.py
+++ postman_py/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for postman-py."""
 
+from postman_py.main import add_attachment, draft_message, send_message, SMTPConf
+
 __author__ = """Alex Macniven"""
 __email__ = "alex_macniven@icloud.com"
 __version__ = "0.1.0"
