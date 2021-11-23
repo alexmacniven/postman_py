fda6e98d203c272982eb61b9919dc02892f0b6e2
Alex Macniven <alex_macniven@icloud.com>
2021-11-23 10:52:14 +0000

    Format Top Level Package Imports

--- postman_py/__init__.py
+++ postman_py/__init__.py
@@ -1,6 +1,14 @@
 """Top-level package for postman-py."""
 
-from postman_py.main import add_attachment, draft_message, send_message, SMTPConf
+# flake8: noqa
+
+from typing import List
+from postman_py.main import (
+    add_attachment,
+    draft_message,
+    send_message,
+    SMTPConf,
+)
 
 __author__ = """Alex Macniven"""
 __email__ = "alex_macniven@icloud.com"
